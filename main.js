b68d9f0108e7f1a0865b3b791156cf3ebc9a5abe
Daniel Ohrin <xqiam@outlook.com>
2022-10-07 09:41:15 -0500

    Initial

---
+++ main.js
@@ -0,0 +1,10 @@
+/* 
+Some properties of his toys that he might want to keep track of:
+-Age group it was made for? num
+-Batteries required? str
+-Batteries included? str
+-Color of the toy? str
+-Model number? str
+-
+I would store all of this data in an object with key value pairs.
+*/
